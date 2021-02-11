4968be85851d8baa0c2a8990aded8880f2042da9
Jose Tapia <jose.tapia@ki.se>
2021-02-11 15:30:33 +0100

    Add new course

---
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_01.r
@@ -0,0 +1,44 @@
+setwd("C:/Users/a-jostap/Documents/R_Projects/Introduction_to_Statistics_and_Data_Analysis")
+
+## From Apendix A
+library(MASS)
+painters
+summary(painters)
+summary(painters$School)
+subset(painters, School=="F")
+painters[painters[["School"]]=="F", c(1,2)]
+subset(painters, Composition <= 6)
+splitted = split(painters, painters$School)
+splitted
+lapply(splitted, summary)
+
+## Exercises
+
+# (b)
+pizza <- read.csv('pizza_delivery.csv')
+fix(pizza)
+View(pizza)
+pizza
+
+# (c)
+pizza2 <- pizza[1:5,1:5]
+pizza2
+
+write.csv(pizza2, file = 'pizza2.csv')
+write.table(pizza2, file = 'pizza2.dat')
+save(pizza2, file = 'pizza2.Rdata')
+
+# (d)
+pizza$NewTemperature <- 32 + 1.8 * pizza$temperature
+
+# (e)
+attach(pizza)
+NewTemperature
+
+# (f)
+str(pizza)
+dim(pizza)
+colnames((pizza))
+names(pizza)
+nrow(pizza)
+
---
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_02.r
@@ -0,0 +1,5 @@
+table(branch)
+table(branch)/length(branch)
+
+sv <- c(rep(1,4), rep(2,16), rep(3,90), rep(4,70), rep(5,20))
+plot.ecdf(sv)
