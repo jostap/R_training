e82165787d3dbc42cc23749688e901e81e174298
Jose Tapia <jose.tapia@ki.se>
2021-02-11 22:27:18 +0100

    Update work directory Windows Pro - Home

--- Introduction_to_Statistics_and_Data_Analysis/chapter_01.r
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_01.r
@@ -1,4 +1,6 @@
-setwd("C:/Users/a-jostap/Documents/Statistical_Analysis/R/Introduction_to_Statistics_and_Data_Analysis")
+#setwd("C:/Users/a-jostap/Documents/Statistical_Analysis/R/Introduction_to_Statistics_and_Data_Analysis")
+
+setwd("C:/Users/joser/Documents/R_Projects/Introduction_to_Statistics_and_Data_Analysis")
 
 ## From Apendix A
 library(MASS)
--- Introduction_to_Statistics_and_Data_Analysis/chapter_02.r
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_02.r
@@ -3,3 +3,8 @@ table(branch)/length(branch)
 
 sv <- c(rep(1,4), rep(2,16), rep(3,90), rep(4,70), rep(5,20))
 plot.ecdf(sv)
+
+barplot(table(branch))
+barplot(table(branch)/length(branch))
+
+pie(table(sv))
