157823acdb389707a023aa5cd6c6e0348a24431e
Jose Tapia <jose.tapia@ki.se>
2021-02-17 00:14:43 +0100

    Complete exercises 2

--- Introduction_to_Statistics_and_Data_Analysis/chapter_02.r
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_02.r
@@ -89,6 +89,14 @@ barplot(results2014,
         xlab="Parties",
         ylab="Votes (%)",
         cex.axis=1.75, lwd=3, cex.lab=1.75, cex.names=1.25)       # to learn more about these options type ?par
+
+barplot(results2009,
+        names.arg=c('ANC',"DA","EFF","IFP","COPE","Others"),
+        col=gray.colors(6),
+        ylim=c(0,0.7),
+        xlab="Parties",
+        ylab="Votes (%)",
+        cex.axis=1.75, lwd=3, cex.lab=1.75, cex.names=1.25)  
 dev.off()
 
 
@@ -126,7 +134,8 @@ plot(density(goals, adjust=1,kernel='gaussian'))
 pdf(file="exercise_2.2_c.pdf")
 par(mar= c(5, 5, 2, 2))
 hist(goals, breaks=c(0,15,30,45,60,75,90,96),col="lightgrey", 
-     cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,ylab="Density",xlab="Time (in min)",xlim=c(0,100),ylim=c(0,0.025),main="")
+     cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,ylab="Density",xlab="Time (in min)",
+     xlim=c(0,100),ylim=c(0,0.025),main="")
 dev.off()
 
 pdf(file="exercise_2.2_d.pdf")
@@ -159,10 +168,11 @@ dev.off()
 
 # not asked in the exercise, but useful to learn more about R; plot ECDF and add lines with the "lines" command
 score <- c(rep(1,1),rep(2,3),rep(3,8),rep(4,8),rep(5,27),rep(6,30),rep(7,11),rep(8,6),rep(9,4),rep(10,2))
-
+score
 pdf(file="exercise_2.5.pdf", width=8)
 par(mar= c(5, 5, 2, 2))
-plot.ecdf(score, cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,xlab="Score",ylab="F(x)",main="",yaxp=c(0,1,10),xaxp=c(0,10,10))
+plot.ecdf(score, cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,
+          xlab="Score",ylab="F(x)",main="",yaxp=c(0,1,10),xaxp=c(0,10,10))
 lines(c(0,5,10),c(0,0.47,1),type="l",col="darkgrey",lwd=3)
 lines(c(3,3),c(0,0.282),type="l",col="darkgrey",lwd=3,lty=2)
 lines(c(9,9),c(0,0.894),type="l",col="darkgrey",lwd=3,lty=2)
@@ -181,30 +191,55 @@ attach(pizza)
 # a)
 pdf(file="exercise_2.6_a.pdf",width=11)
 par(mar= c(5, 5, 2, 2))
-hist(temperature, cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,xlab="Temperature",main="",xlim=c(40,90),ylim=c(0,400),col="lightgrey",ylab="Number of deliveries")
+hist(temperature, cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,
+     xlab="Temperature",main="",xlim=c(40,90),ylim=c(0,400),col="lightgrey",ylab="Number of deliveries")
 lines(c(65,65),c(0,400),type="l",lty=2,lwd=3)
 dev.off()
 
 # b)  [the "theme" option is to improve labelling and font size, not necessarily needed]
 pdf(file="exercise_2.6_b.pdf",width=11)
 p1 <- ggplot(data=pizza,aes(x=temperature))
-p2 <- p1 + geom_histogram(fill="darkgrey",alpha=0.5,binwidth=2.5,)  + scale_y_continuous("Number of deliveries") + scale_x_continuous("Temperature")
-plot(p2  + theme(axis.title.x = element_text(size=20),axis.title.y = element_text(size=20), axis.text.x = element_text(size=14), axis.text.y = element_text(size=14)))
+p2 <- p1 + geom_histogram(fill="darkgrey",alpha=0.5,binwidth=2.5,) + 
+  scale_y_continuous("Number of deliveries") + scale_x_continuous("Temperature")
+
+plot(p2 + theme(axis.title.x = element_text(size=20),
+                axis.title.y = element_text(size=20), 
+                axis.text.x = element_text(size=14), 
+                axis.text.y = element_text(size=14)))
+plot(p1)
+plot(p2)
 dev.off()
 
 # c)
+driver
+?barplot
+?par
+
 pdf(file="exercise_2.6_c.pdf",width=11)
 par(mar= c(5, 5, 2, 2))
-barplot(table(driver),ylim=c(0,400),ylab="Number of deliveries", xlab="Driver",col=gray.colors(7),main="Title", cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,cex.names=1.5)
+barplot(table(driver),ylim=c(0,400),
+        ylab="Number of deliveries", 
+        xlab="Driver",
+        col=gray.colors(7),
+        main="Title", cex.axis=1.75,lwd=3,cex.lab=1.75,cex.main=1.75,cex.names=1.5)
 dev.off()
 
 
 # d)  [the "theme" option is to improve labelling and font size, not necessarily needed]
-qplot(driver,data=pizza,aes="bar")
+qplot(driver,data=pizza)
 
 pdf(file="exercise_2.6_d.pdf",width=11)
-p3 <- qplot(driver,data=pizza,aes=("bar"),fill=day)
-p4 <- p3 + scale_fill_grey() +theme_bw() + scale_y_continuous("Number of deliveries") + scale_x_discrete("Driver") 
-plot(p4+ theme(axis.title.x = element_text(size=20),axis.title.y = element_text(size=20), legend.title =  element_text(size=18, hjust = 0), legend.text =  element_text(size=18), axis.text.x = element_text(size=16), axis.text.y = element_text(size=16)))
+p3 <- qplot(driver,data=pizza,fill=day)
+p4 <- p3 + scale_fill_grey() +theme_bw() + 
+  scale_y_continuous("Number of deliveries") + 
+  scale_x_discrete("Driver") 
+plot(p4 + theme(axis.title.x = element_text(size=20),
+               axis.title.y = element_text(size=20), 
+               legend.title =  element_text(size=18, hjust = 0), 
+               legend.text =  element_text(size=18), 
+               axis.text.x = element_text(size=16), axis.text.y = element_text(size=16)))
+
+plot(p3)
+plot(p4)
 dev.off()
 
