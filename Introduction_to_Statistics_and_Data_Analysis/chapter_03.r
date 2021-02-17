21cb11534c618fa68e6586e8cc8bc93bfe7dfbdf
Jose Tapia <jose.tapia@ki.se>
2021-02-18 00:14:38 +0100

    Add chapter 3 file

---
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_03.r
@@ -0,0 +1,30 @@
+weather <- c(22, 24, 21, 22, 25, 26, 25, 24, 23, 25, 25, 26, 27, 25, 26,
+             25, 26, 27, 27, 28, 29, 29, 29, 28, 30, 29, 30, 31, 30, 28, 29)
+weather
+mean(weather)
+
+
+weighted.mean(c(22.5,27.5,32.5),c(12/31,18/31,1/31))
+median(weather)
+quantile(weather)
+quantile(weather, probs = c(0, 0.25, 0.5, 0.75,1))
+?qqplot
+
+y <- pizza[pizza[,6] == 'Domenico',] 
+y$time
+
+x <- pizza[pizza[,6] == 'Luigi',] 
+x$time
+par(mar= c(5, 5, 2, 2))
+qqplot(x$time, y$time, xlim=c(10,60),ylim=c(10,60), xlab = 'Delivery time from Luigi', ylab = 'Delivery time from Domenico')
+lines(c(10,60),c(10,60),type="l",col="blue",lwd=2)
+
+z <- pizza[pizza[,6] == 'Mario',] 
+z$time
+
+w <- pizza[pizza[,6] == 'Salvatore',] 
+w$time
+
+par(mar= c(5, 5, 2, 2))
+qqplot(z$time, w$time, xlim=c(10,60),ylim=c(10,60), xlab = 'Delivery time from Mario', ylab = 'Delivery time from Salvatore')
+lines(c(10,60),c(10,60),type="l",col="blue",lwd=2)
