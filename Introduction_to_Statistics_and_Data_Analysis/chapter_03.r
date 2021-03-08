6d7980a0be380cc2a9b3e3572ba27a39d56e708b
Jose Tapia <jose.tapia@ki.se>
2021-03-08 01:02:02 +0100

    Add Chap 3 Exercises

--- Introduction_to_Statistics_and_Data_Analysis/chapter_03.r
+++ Introduction_to_Statistics_and_Data_Analysis/chapter_03.r
@@ -52,5 +52,49 @@ plot(Lc(x))
 
 plot(Lc(time))
 
+################
+# Exercise 3.1 #
+################
 
+distance <- c(12.5,29.9,14.8,18.7,7.6,16.2,16.5,27.4,12.1,17.5)
+altitude <- c(342,1245,502,555,398,670,796,912,238,466)
+
+# a) 
+mean(distance)
+mean(altitude)
+
+median(distance)
+median(altitude)
+
+# b)
+quantile(distance,probs=0.75)
+quantile(distance,probs=0.25)
+quantile(altitude,probs=0.75)
+quantile(altitude,probs=0.25)
+
+# c)
+quantile(distance,probs=0.75)-quantile(distance,probs=0.25)
+quantile(altitude,probs=0.75)-quantile(altitude,probs=0.25)
+
+amd <- function(mv){1/length(mv)*sum(abs(mv-median(mv)))}
+amd(distance)
+amd(altitude)
+
+var(altitude)
+var(distance)
+
+# e)
+
+pdf(file="exercise_3.1_e1.pdf")
+par(mar= c(5, 5, 2, 2))
+boxplot(altitude,xlab="",ylab="Altitude (in m)", cex.axis=1.5,lwd=3,cex.lab=1.75,cex.main=1.75) 
+dev.off()
+
+pdf(file="exercise_3.1_e2.pdf")
+par(mar= c(5, 5, 2, 2))
+boxplot(distance,xlab="",ylab="Distance (in km)", cex.axis=1.5,lwd=3,cex.lab=1.75,cex.main=1.75) 
+dev.off()
+
+# f)
+weighted.mean(c(10,17.5,25),c(4/10,4/10,2/10))
 
